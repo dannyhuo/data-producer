019c356cf979409ffe7fc8a1fe5202a7c919d43b
javatech@163.com <javatech@163.com>
2020-03-18 10:40:04 +0800

    时区设置

--- src/main/java/com/data/dataproducer/DataProducerApplication.java
+++ src/main/java/com/data/dataproducer/DataProducerApplication.java
@@ -7,12 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @MapperScan("com.data.dataproducer.mapper")
 @SpringBootApplication(scanBasePackages = {"com.data.dataproducer"})
 public class DataProducerApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+		
 		SpringApplication.run(DataProducerApplication.class, args);
 	}
 
